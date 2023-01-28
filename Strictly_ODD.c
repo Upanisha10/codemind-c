2518ea73c6df9389b460ab8a35a05854d80a2495
Upanisha10 <120163856+Upanisha10@users.noreply.github.com>
2023-01-28 22:15:29 +0530

    #include<stdio.h> int main() {     int n,i,d,k,c,b,flag=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             if(i%2==0)             {               flag=0;               printf("False");               break;             }             else             flag=1;         }     }     if(flag)     {         printf("True");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,d,k,c,b,flag=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            if(i%2==0)
+            {
+              flag=0;
+              printf("False");
+              break;
+            }
+            else
+            flag=1;
+        }
+    }
+    if(flag) 
+    {
+        printf("True");
+    }
+}
